d08312a5ced9638fae953f48da7fb9de3f952bad
BmKo <60381441+BmKo@users.noreply.github.com>
2023-09-22 19:34:28 +1200

    Call register endpoint on submit and add app to cors on server

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,40 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
+
+const url = process.env.SERVER_URL || 'http://localhost:3000';
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    let {username, email, password} = Object.fromEntries(data.entries());
+
+    fetch(`${url}/auth/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({username, email, password})
+    }).then(res => {
+        console.log(`res.status: ${res.status}`)
+        if (res.status === 201) {
+            alert(`${username} registered to database.`);
+            return res.json();
+        } else {
+            alert(`Error registering user: ${res.status}`);
+            return res.json();
+        }
+    }).catch(err => {
+        console.log(err);
+        alert("Error registering user.");
+    });
+}
 
 function App() {
     return (
         <div className="App">
             <div className="App-header">
-                <form>
+                <form onSubmit={handleSubmit} method="post">
                     <h2>Register Account</h2>
                     <div>
                         <label>Username: </label>
@@ -13,14 +42,14 @@ function App() {
                     </div>
                     <div>
                         <label>Email: </label>
-                        <input className="input-field"  type="email" name="email" required/>
+                        <input className="input-field" type="email" name="email" required/>
                     </div>
                     <div>
                         <label>Password: </label>
-                        <input className="input-field"  type="password" name="password" required/>
+                        <input className="input-field" type="password" name="password" required/>
                     </div>
                     <div>
-                        <input className="input-button"  type="submit" value="Register"/>
+                        <input className="input-button" type="submit" value="Register"/>
                     </div>
                 </form>
             </div>
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,14 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const cors = require('cors');
+require('dotenv').config();
+
+const appUrl = process.env.APP_URL || 'http://localhost:3005';
+
+router.use(cors({
+    origin: appUrl,
+}))
 
 // Registration
 router.post('/register', authController.register);
