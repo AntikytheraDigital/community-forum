8718f5b06573b5f026a0df46b88a2e9684b31803
pastacomposer <pmvwalter@gmail.com>
2023-09-25 16:02:48 +1300

    1 test successful: register user. IDK why test script keeps running.

--- server/app.js
+++ server/app.js
@@ -2,27 +2,15 @@ const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
 const authRoutes = require('./routes/auth');
-const mongoose = require("mongoose");
-
+const {connectDatabase} = require("./models/database");
 require('dotenv').config();
-
 const app = express();
-
-if (process.env.ATLAS_URI) {
-    mongoose.connect(process.env.ATLAS_URI)
-} else {
-    throw 'configure ATLAS_URI in .env'
-}
-const db = mongoose.connection
-db.on('error', err => {
-    console.log(`There was an error connecting to the database: ${err}`)
-})
-db.once('open', () => {
-    console.log(`Successfully connected to database`)
-})
+connectDatabase().then(() => {
+    console.log("Database connected");
+});
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: process.env.SESSION_KEY, resave: false, saveUninitialized: false }));
+app.use(express.urlencoded({extended: true}));
+app.use(session({secret: process.env.SESSION_KEY, resave: false, saveUninitialized: false}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -30,6 +18,8 @@ app.use(passport.session());
 app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+
+module.exports = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ server/models/database.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const {MongoMemoryServer} = require('mongodb-memory-server');
+
+let mockDatabase, dbUrl = null;
+
+const connectDatabase = async () => {
+    try {
+        if (process.env.NODE_ENV === 'test') {
+            // Use in-memory database for testing
+            // this can fail on UBUNTU 22.04, fix by adding environment variable: MONGOMS_VERSION=6.0.4
+            mockDatabase = await MongoMemoryServer.create();
+            dbUrl = mockDatabase.getUri();
+        } else if (process.env.ATLAS_URI) {
+            // Use production database
+            dbUrl = process.env.ATLAS_URI;
+        } else {
+            // environment variable not set
+            throw ('Environment variable ATLAS_URI must be set');
+        }
+
+        const db = await mongoose.connect(dbUrl);
+
+        console.log(`MongoDB connected: ${db.connection.host}`);
+    }
+    catch(err) {
+        console.log(`MongoDB connection error: ${err}`);
+        process.exit(1);
+    }
+}
+const disconnectDatabase = async () => {
+    try {
+        await mongoose.connection.close();
+        if (mockDatabase) {
+            await mockDatabase.stop();
+        }
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+module.exports = {connectDatabase, disconnectDatabase};
