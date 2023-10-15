30fb5c3f265bfe4f2f5966a5b8f158a5ab2dcf44
BmKo <60381441+BmKo@users.noreply.github.com>
2023-10-16 09:04:35 +1300

    Add temporary comments for testing

--- app/src/controllers/postController.js
+++ app/src/controllers/postController.js
@@ -2,6 +2,7 @@ const url = process.env.SERVER_URL || 'http://localhost:3000';
 
 async function handleGetPost(postID){
     try {
+        // TODO: Update fetch to use the correct endpoint
         const response = await fetch(`${url}/posts/x?id=${postID}`, {
             method: 'GET'
         });
@@ -11,7 +12,22 @@ async function handleGetPost(postID){
         let date = new Date(post["timestamp"]);
         post["timestamp"] = date.toLocaleString();
 
-        console.log(post)
+        // TODO: Temporarily add comments to post
+        post["comments"] = [
+            {
+                _id: "251a39cb4682dc4660ac2519",
+                authorID: "FunnyGuy4",
+                timestamp: "2023-10-04T01:15:12.630+00:00",
+                content: "This is a comment."
+            },
+            {
+                _id: "551a39cb4682dc4660ac2519",
+                authorID: "NotFunnyGuy",
+                timestamp: "2023-10-04T02:15:12.630+00:00",
+                content: "This is another less funny comment."
+            }
+        ]
+
         return post;
     } catch {
         console.log("Error getting post.");
