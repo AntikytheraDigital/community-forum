c7c1dbc817013b9977c4782de45803d322915db4
BmKo <60381441+BmKo@users.noreply.github.com>
2023-10-16 08:54:50 +1300

    Add get post endpoint and format post

--- app/src/controllers/postController.js
+++ app/src/controllers/postController.js
@@ -1,7 +1,22 @@
 const url = process.env.SERVER_URL || 'http://localhost:3000';
 
 async function handleGetPost(postID){
-    return {error: "Not implemented."};
+    try {
+        const response = await fetch(`${url}/posts/x?id=${postID}`, {
+            method: 'GET'
+        });
+
+        let json = await response.json();
+        let post = json["post"];
+        let date = new Date(post["timestamp"]);
+        post["timestamp"] = date.toLocaleString();
+
+        console.log(post)
+        return post;
+    } catch {
+        console.log("Error getting post.");
+        return {error: "Error getting post."}
+    }
 }
 
 module.exports = {
